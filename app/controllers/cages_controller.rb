23df269f356e5f8c5f8e5b481631b04fd2d92bc5
Rob Hill <robert.hill0122@gmail.com>
2014-08-25 10:22:04 -0600

    User can add a new Cage movie to db and it will be displayed on root

--- app/controllers/cages_controller.rb
+++ app/controllers/cages_controller.rb
@@ -1,11 +1,24 @@
 class CagesController < ApplicationController
 
   def index
-
+    @cages = Cage.all
   end
 
   def new
     @cage = Cage.new
+  end
+
+  def create
+    @cage = Cage.new(name: params[:cage][:name],
+                     year: params[:cage][:year],
+                     synopsis: params[:cage][:synopsis]
+    )
+
+    if @cage.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 end
